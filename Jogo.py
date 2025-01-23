ad89068005852d1e6bdd8b47ca39dafe4e939580
waguinhocm <waguinhocm@hotmail.com>
2025-01-23 20:07:01 -0300

    Prototipo do Jogo de Perguntas e Respostas

---
+++ Jogo.py
@@ -0,0 +1,20 @@
+### Jogo de Perguntas e Respostas ###
+
+print("Vamos Começar? Quero ver se você é bom em matemática! Vou te dar três chances.")
+chance = 0
+while chance <= 3:
+
+    try:
+        print("""Atenção à pergunta:
+            
+        Qual é a soma de 37 + 26? """)
+        resposta = int(input("Responda: "))
+        if resposta == (37+26):
+            print("Parabéns! Acertou a tentativa de número - ", + (chance + 1))
+            break
+    except:
+        print("Tente novamente.")
+    chance += 1
+
+if chance == 3:
+    print("Perdeu as três chances!")
