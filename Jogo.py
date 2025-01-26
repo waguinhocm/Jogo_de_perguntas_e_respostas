ef315c957bd2788b5c0aef626199f6a56b914da9
waguinhocm <waguinhocm@hotmail.com>
2025-01-26 10:59:33 -0300

    Incluindo pontuação

--- Jogo.py
+++ Jogo.py
@@ -2,19 +2,26 @@
 
 print("Vamos Começar? Quero ver se você é bom em matemática! Vou te dar três chances.")
 chance = 0
+pontos = 0
 while chance < 3:
-
-    try:
-        print("""Atenção à pergunta:
+    perguntas = ['Qual é a soma de 37 + 26?', 'Qual é o resultado de  13 X 17?', 'Quanto é 56 - 18?']
+    respostas = ["63" , "221" , "38"]
+    if pontos == (len(perguntas)*10):
+        print(f"Parabéns!! Acertou todas as perguntas e fez {pontos}!!")
+        break
+    for i in range(len(perguntas)):
+        print(perguntas[i])
+        respostaUsuario = str(input("Responda: "))
+        
+        if respostaUsuario != respostas[i]:
+            if chance < 3:
+                chance += 1
+                pontos = 0
+                print(f"Perdeu a {chance}ª chance!")
             
-        Qual é a soma de 37 + 26? """)
-        resposta = int(input("Responda: "))
-        if resposta == (37+26):
-            print("Parabéns! Acertou a tentativa de número - ", + (chance + 1))
             break
-    except:
-        print("Tente novamente.")
-    chance += 1
-
-if chance == 3:
-    print("Perdeu as três chances!")
+        else:
+            pontos += 10
+            
+if chance >= 3:
+    print("Que pena! Precisamos revisar os estudos!")
